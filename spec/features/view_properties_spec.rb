484fb8f851544826bdc28079346419a534c6286c
Leslie <leslie.liszcz@gmail.com>
2021-06-30 15:24:07 +0100

    Updated listings

--- spec/features/view_properties_spec.rb
+++ spec/features/view_properties_spec.rb
@@ -2,6 +2,8 @@ require 'property'
 
 feature 'Viewing properties' do
   scenario 'A user can see the properties' do
+    connection = PG.connect(dbname: 'makersbnb_test')
+    
     Property.create(name: "Riverside", description: "Cabin with a river view", location: "River City", price: 100)
     Property.create(name: "Lakeside", description: "Cabin on the lake with a boat", location: "Lake City", price: 150)
     Property.create(name: "Oceanside", description: "Studio on the beach", location: "Ocean City", price: 200)
