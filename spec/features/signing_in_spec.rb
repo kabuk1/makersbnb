a0d682cddd405820b7f508b551f8153037825fab
Leslie <leslie.liszcz@gmail.com>
2021-07-01 17:16:25 +0100

    updated navigation

--- spec/features/signing_in_spec.rb
+++ spec/features/signing_in_spec.rb
@@ -1,15 +1,13 @@
 feature "authentication" do
   scenario "a user can sign-in" do
     User.create(email: "test@example.com", password: "password123")
-    visit "/"
-    click_button("Sign up")
-    click_link("Login")
 
     visit "/login"
     fill_in("email", with: "test@example.com")
     fill_in("password", with: "password123")
     click_button("Login")
     
+    expect(page).to have_current_path '/users'
     expect(page).to have_content "Welcome, test@example.com"
   end
 end
